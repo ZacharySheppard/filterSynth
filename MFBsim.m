338aa64fc09a712e6b3bf638dcd33d506e512510
ZacharySheppard <zacharysheppard813@gmail.com>
2020-03-05 16:40:12 -0330

    added step response

--- MFBsim.m
+++ MFBsim.m
@@ -1,26 +1,58 @@
-%%component Values
+%% Component Values
+close all
 
-c1 = 28.1*10^-9;
-c2 = 402*10^-12;
+c1 = 25.5*10^-9;
+c2 = 397.8*10^-12;
 r1 = 10*10^3;
 r2 = 10*10^3;
-r3 = 4.62*10^3;
+r3 = 5*10^3;
 
 g1 = 1/r1;
 g2 = 1/r2;
 g3 = 1/r3;
 
+%% Design Equations
+%Design Equations for Rauch LPF
+omega = 1/sqrt(r1*r2*c1*c2);
+Q = omega*c1/(g1+g2+g3);
+H = r1/r3;
 
-w = 1:1e6;
+%% Transfer Function Generation
+w = 1:1e6;     %frequency range
 
-
-num = -(r1/r3)*(1/(c1*c2*r1*r2));
-
-b = (g1+g2+g3)/c1;
-c = 1/(r1*r2*c2*c1);
-
-Ts1 =       num ./ ...
-      (w.^2 + b*w + c);
-  
-semilogx(-Ts1)
+Ts =                H*omega^2 ./ ...
+     ( (1i*w).^2 + (omega/Q).*(1i*w) + omega^2);
+ 
+%% Magnitude and Phase Plot
+figure(1);
+semilogx(abs(Ts))
+hold on
 grid on
+xlabel('Frequency (rad/s)')
+ylabel('Magnitude (|T(jw)|')
+title('Magnitude Plot for Rauch LPF')
+
+figure(2);
+semilogx(angle(Ts)*180/pi)
+hold on
+grid on
+xlabel('Frequency (rad/s)')
+ylabel('Phase Angle (degeres)')
+title('Phase Angle Plot for Rauch LPF')
+%% Step Response 
+
+syms T(s) F(t)
+T(s) =       H*omega^2 ./ ...
+      (s.*((s).^2 + (omega/Q).*(s) + omega^2));
+  
+%% Transient Response
+F(t) = ilaplace(T(s));
+figure(3);
+time = 0:0.00001:0.001;
+transient = double(F(time));
+plot(time,transient)
+xlabel('Time (s)')
+ylabel('Magnitude (|T(t)|')
+title('Transient Step Response Plot for Rauch LPF')
+
+
--- sallenkeysim.m
+++ sallenkeysim.m
@@ -1,26 +1,54 @@
-%%component Values
+%% component Values
 
 
-c1 = 9.71*10^-9;
-c2 = 9.94*10^-9;
-r1 = 3.27*10^3;
-r2 = 3.28*10^3;
+c1 = 10*10^-9;
+c2 = 10*10^-9;
+r1 = 3.2*10^3;
+r2 = 3.2*10^3;
 g1 = 1/r1;
 g2 = 1/r2;
-ra = 9.93*10^3;
-rb = 9.90*10^3;
-
-w = 1:100e6;
+ra = 10*10^3;
+rb = 10*10^3;
+%% Design Equations
 
 k = 1+(rb/ra);
+omega = 1/sqrt(r1*r2*c1*c2);
+Q = 1/(3-k);
+H = k;
+%% Transfer Function Generation
+w = 1:100e6;
+Ts =        H*omega^2./ ...
+      ((1i*w).^2 + (omega/Q)*(1i*w) + omega^2);
 
-num = k/(c1*c2*r1*r2);
-
-b = ((g1+g2)/c1) + ((1-k)/(r2*c2));
-c = 1/(c1*c2*r1*r2);
-
-Ts1 =       num ./ ...
-      (w.^2 + b*w + c);
-  
-semilogx(Ts1)
+%% Magnitude and Phase Plot
+figure(1);
+semilogx(abs(Ts))
+hold on
 grid on
+xlabel('Frequency (rad/s)')
+ylabel('Magnitude (|T(jw)|')
+title('Magnitude Plot for Sallen-Key LPF')
+
+figure(2);
+semilogx(angle(Ts)*180/pi)
+hold on
+grid on
+xlabel('Frequency (rad/s)')
+ylabel('Phase Angle (degeres)')
+title('Phase Angle Plot for Sallen-Key LPF')
+
+%% Step Response 
+
+syms T(s) F(t)
+T(s) =       H*omega^2 ./ ...
+      (s.*((s).^2 + (omega/Q).*(s) + omega^2));
+  
+%% Transient Response
+F(t) = ilaplace(T(s));
+figure(3);
+time = 0:0.00001:0.001;
+transient = double(F(time));
+plot(time,transient)
+xlabel('Time (s)')
+ylabel('Magnitude (|T(t)|')
+title('Transient Step Response Plot for Sallen-Key LPF')
